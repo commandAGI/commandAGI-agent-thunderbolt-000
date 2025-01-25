c370b405dd1dec2210bb6aad36121be584f9332e
Jacob Valdez <jacobfv123@gmail.com>
2025-01-25 03:54:48 -0600

    init

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+class ThunderboltAgent {
+    constructor() {}
+
+    sendScreenshot(screenshot: Screenshot) {
+        // use VLM to generate message to append to the perception context
+        // also generate OCR record of the screenshot and append to the perception context
+    }
+
+    sendImageMessage(image: Image) {
+        // use VLM to generate message to append to the perception context
+    }
+
+    sendTextMessage(message: string) {
+        // directly append to the perception context
+    }
+
+    think() {
+        // tell it to think and to make a plan or ensure its current plan is still valid
+    }
+
+    getAction() {
+        // ask deepseek-v0 to generate a list of actions
+        // - the get_screenshot tool yields execution and just returns until the next screenshot is available and the actions are recalculated
+        // - actions involving the mouse are sent to UI TARS to get exact coordinates
+        // - other actions are directly returned as tool calls without additional processing
+    }
+
+    start() {
+        // start agent
+    }
+
+    stop() {
+        // stop agent
+    }
+}
